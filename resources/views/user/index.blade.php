097baec25bc95f6cb709097afd7d0096f61d482a
Dreknor <pauli84@freenet.de>
2020-07-03 16:03:27 +0200

    export user table

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -108,9 +108,21 @@
 
 @push('js')
  <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+ <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+ <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+ <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
+
+
  <script>
      $(document).ready( function () {
-         $('#userTable').DataTable();
+         $('#userTable').DataTable( {
+             dom: 'Bfrtip',
+             buttons: [
+                  'excel', 'pdf', 'print'
+             ]
+         } );
      } );
  </script>
 
