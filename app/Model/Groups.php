2517a2f0efdd7aa3ecaec09e7b0aa0747dce9562
Dreknor <pauli84@freenet.de>
2020-08-29 18:41:10 +0200

    update

---
+++ app/Model/Groups.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+
+class Groups extends Model implements HasMedia
+{
+
+    use HasMediaTrait;
+
+
+    protected $fillable = ['name', 'bereich', 'protected'];
+    protected $visible = ['name', 'bereich', 'protected'];
+
+    protected $casts = [
+      "protected" => "boolean"
+    ];
+
+    public function users (){
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+
+    public function posts(){
+        return $this->belongsToMany(Posts::class)->withTimestamps();
+    }
+
+    public function termine(){
+        return $this->belongsToMany(Termin::class);
+    }
+}
---
+++ app/Model/Posts.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Model;
+
+use Artisanry\Commentable\Traits\HasComments;
+use Bkwld\Cloner\Cloneable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\Models\Media;
+
+
+class Posts extends Model  implements HasMedia
+{
+    use HasMediaTrait;
+    use SoftDeletes;
+    use Cloneable;
+    use HasComments;
+
+    use \Staudenmeir\EloquentHasManyDeep\HasRelationships;
+
+
+    protected $fillable = ['header', 'news', 'released', 'author', 'archiv_ab', 'type'];
+
+    protected $dates = ['created_at', 'updated_at', 'archiv_ab'];
+
+    protected $cloneable_relations = ['groups', 'rueckmeldung'];
+
+
+    protected $with= ['rueckmeldung'];
+
+    public function groups()
+    {
+        return $this->belongsToMany(Groups::class);
+    }
+
+    public function autor(){
+        return $this->hasOne(User::class, 'id', 'author');
+    }
+
+    public function rueckmeldung(){
+        return $this->hasOne(Rueckmeldungen::class);
+    }
+
+
+    public function userRueckmeldung(){
+        return $this->hasMany(UserRueckmeldungen::class);
+    }
+
+    public function users(){
+        return $this->hasManyDeep('App\Model\User', ['groups_posts', 'App\Model\Groups','groups_user']);
+    }
+
+}
