ddb29713d6087397e52b820202c4e9993838025a
Dreknor <Dreknor@users.noreply.github.com>
2021-01-26 16:23:32 +0100

    First user
    setup first user and permissions

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,13 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        \Illuminate\Support\Facades\DB::table('users')->insert([
+            'name'=>env('MAIL_FROM_NAME') ,
+            'email'=>env('MAIL_FROM_ADDRESS') ,
+            'password'=> \Illuminate\Support\Facades\Hash::make(\Carbon\Carbon::now()->format('dmY')),
+            'changeSettings'    => 0
+        ]);
     }
 
     /**
