d96e52c5c430bec6647a2220b608fc9a58a98b7e
Laravel Shift <shift@laravelshift.com>
2021-03-05 18:36:37 +0000

    Convert `env()` to `config()`
    Laravel [recommends][1] only using `env()` within configuration files and using `config()` everywhere else. Doing so allows you to improve performance by running `php artisan config:cache`.
    
    [1]: https://laravel.com/docs/5.7/configuration#configuration-caching

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,7 @@
                         <span class="navbar-toggler-bar bar3"></span>
                     </button>
                 </div>
-                <a class="navbar-brand" href="{{url('/')}}">{{env('APP_NAME')}}</a>
+                <a class="navbar-brand" href="{{url('/')}}">{{config('app.name')}}</a>
             </div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-bar navbar-kebab"></span>
