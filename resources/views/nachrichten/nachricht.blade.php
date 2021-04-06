671e89610ebb106930eb81e1e0ec88e5ace544af
Dreknor <pauli84@freenet.de>
2021-04-06 06:54:12 +0200

    test blur

--- resources/views/nachrichten/nachricht.blade.php
+++ resources/views/nachrichten/nachricht.blade.php
@@ -1,9 +1,8 @@
 
-<div class="nachricht  {{$nachricht->type}} card @if($nachricht->released == 0) border border-info @endif" id="{{$nachricht->id}}">
-    @if(count($nachricht->getMedia('header'))>0)
-            <img class="card-img-top" src="{{url('/image/'.$nachricht->getMedia('header')->first()->id)}}" style="max-height: 250px;object-fit: cover; object-position: 0 70%;">
-    @endif
-    <div class=" @if($nachricht->released == 0) bg-info @endif card-header border-bottom" >
+<div class="nachricht  {{$nachricht->type}} card @if($nachricht->released == 0) border border-info @endif" id="{{$nachricht->id}}" @if(count($nachricht->getMedia('header'))>0) style="background-image: url({{url('/image/'.$nachricht->getMedia('header')->first()->id)}}); background-size: cover;" @endif>
+
+    <div class=" @if($nachricht->released == 0) bg-info @endif card-header border-bottom blur" >
+
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-10">
@@ -104,7 +103,7 @@
         </div>
 
     </div>
-    <div class="card-body @if($nachricht->is_archived) collapse @endif" id="Collapse{{$nachricht->id}}">
+    <div class="card-body  @if($nachricht->is_archived) collapse @endif" id="Collapse{{$nachricht->id}}">
         <div class="container-fluid">
             @if(count($nachricht->getMedia('images'))>0 or count($nachricht->getMedia('files'))>0)
                 <div class="row">
