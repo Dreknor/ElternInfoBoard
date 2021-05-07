58a5f1d926254d8fdd054a159720f275cc512462
Dreknor <Dreknor@users.noreply.github.com>
2021-05-07 17:53:36 +0200

    Update LastOnlineAt.php

--- app/Http/Middleware/LastOnlineAt.php
+++ app/Http/Middleware/LastOnlineAt.php
@@ -16,15 +16,15 @@ class LastOnlineAt
      */
     public function handle($request, Closure $next)
     {
-        if (auth()->guest() or $request->session()->has('ownID')) {
+        if (auth()->guest() or session()->has('ownID')) {
             return $next($request);
         }
-        if (auth()->user()->last_online_at->diffInMinutes(now()) <= 5 and auth()->user()->track_login == true) {
-            DB::table('users')
-                ->where('id', auth()->user()->id)
-                ->update(['last_online_at' => now()]);
+        if (auth()->user()->last_online_at->diffInMinutes(now()) <= 5 and auth()->user()->track_login == true)
+        {
+            DB::table("users")
+                ->where("id", auth()->user()->id)
+                ->update(["last_online_at" => now()]);
         }
-
         return $next($request);
     }
 }
