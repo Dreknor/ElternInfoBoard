616f458170928b8df79406e2bbb7b3b56dab921e
Dreknor <Dreknor@users.noreply.github.com>
2021-05-07 17:59:42 +0200

    Update LastOnlineAt.php

--- app/Http/Middleware/LastOnlineAt.php
+++ app/Http/Middleware/LastOnlineAt.php
@@ -4,27 +4,29 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class LastOnlineAt
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Illuminate\Support\Facades\Request  $request
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         if (auth()->guest() or session()->has('ownID')) {
             return $next($request);
         }
-        if (auth()->user()->last_online_at->diffInMinutes(now()) <= 5 and auth()->user()->track_login == true)
+        if (auth()->user()->last_online_at->diffInMinutes(now()) >= 5 and auth()->user()->track_login == true)
         {
             DB::table("users")
                 ->where("id", auth()->user()->id)
                 ->update(["last_online_at" => now()]);
         }
+
         return $next($request);
     }
 }
