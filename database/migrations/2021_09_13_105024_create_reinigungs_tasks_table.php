5744afaaa67697a4095311e8249dc09839c7577f
Dreknor <Dreknor@users.noreply.github.com>
2021-09-13 11:30:02 +0200

    change ReinigungsTasks to database -> create new one, delete is ToDo

---
+++ database/migrations/2021_09_13_105024_create_reinigungs_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReinigungsTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reinigungs_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('task', 60);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reinigungs_tasks');
+    }
+}
