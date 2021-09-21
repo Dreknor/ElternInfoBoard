8945185e01750d38db2ea0b5a154918a31a59e35
Dreknor <Dreknor@users.noreply.github.com>
2021-09-21 08:37:22 +0200

    change iframe size

--- resources/views/vertretungsplan/index.blade.php
+++ resources/views/vertretungsplan/index.blade.php
@@ -3,6 +3,6 @@
 
 @section('content')
     <div class="container-fluid">
-        <iframe src="{{config('app.mitarbeiterboard').'/vertretungsplan'.$gruppen}}" width="100%" height="600px" frameborder="0"></iframe>
+        <iframe src="{{config('app.mitarbeiterboard').'/vertretungsplan'.$gruppen}}" width="100%" height="800px" frameborder="0"></iframe>
     </div>
 @endsection
