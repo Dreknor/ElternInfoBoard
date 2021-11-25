c2d6246338459048abf025ac919a2a80d8bf1b9a
Dreknor <Dreknor@users.noreply.github.com>
2021-11-25 11:41:31 +0100

    create Reactions

--- resources/views/nachrichten/footer/reactions.blade.php
+++ resources/views/nachrichten/footer/reactions.blade.php
@@ -15,7 +15,7 @@
             <div class="col-auto">
                 <a href="{{url('posts/'.$nachricht->id.'/react/happy')}}"
                    class="card-link reaction @if($nachricht->reacted()) reacted @endif">
-                    <i class="far fa-sad-tear @if($nachricht->reacted()) reacted @endif"></i>
+                    <i class="far fa-smile @if($nachricht->reacted()) reacted @endif"></i>
                     @if($nachricht->reactions->count() > 0)
                         {{($nachricht->reactions->where('name', 'happy')->count()/$nachricht->reactions->count())*100}}
                         %
