13452da91ac505d88cb2796f7d2bdf2309449dd6
Dreknor <Dreknor@users.noreply.github.com>
2022-04-06 21:23:52 +0200

    Änderung Vertretungsplan

--- resources/views/vertretungsplan/index.blade.php
+++ resources/views/vertretungsplan/index.blade.php
@@ -2,7 +2,96 @@
 @section('title') - Vertretungsplan @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <iframe src="{{config('app.mitarbeiterboard').'/vertretungsplan'.$gruppen}}" width="100%" height="800px" frameborder="0"></iframe>
+    <div class="content d-none d-lg-block">
+        @for($x=Carbon\Carbon::today(); $x< $targetDate; $x->addDay())
+            @if(!$x->isWeekend())
+                <div class="card border border-dark">
+                    <div class="card-header" id="heading{{$x->format('Ymd')}}">
+                        <h6>
+                            Vertretungen für
+                            <div class="text-danger d-inline">{{$x->locale('de')->dayName}} </div>
+                            , den {{$x->format('d.m.Y')}}
+                        </h6>
+                    </div>
+                    <div id="collapse{{$x->format('Ymd')}}" aria-labelledby="heading{{$x->format('Ymd')}}">
+                        <div class="card-body">
+                            <div class="">
+                                <table class="table table-bordered">
+                                    <thead class="thead-light">
+                                    <tr class="">
+                                        <th class="d-lg-table-cell">Klasse</th>
+                                        <th class="d-lg-table-cell">Stunde</th>
+                                        <th class="d-lg-table-cell">Fächer</th>
+                                        <th class="d-none d-lg-table-cell">Lehrer</th>
+                                        <th class="d-none d-lg-table-cell">Kommentar</th>
+                                    </tr>
+                                    <tr class="d-lg-none">
+                                        <th class="d-lg-table-cell">Lehrer</th>
+                                        <th class="d-lg-table-cell" colspan="2">Kommentar</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($vertretungen->filter(function ($vertretung) use ($x) {
+                                        if (\Carbon\Carbon::make($vertretung->date)->eq($x)){
+                                            return $vertretung;
+                                        }
+                                    }) as $vertretung)
+                                        <tr @if(($loop->iteration-1)%2 == 0) class="bg-secondary text-white" @endif>
+                                            <td class="d-lg-table-cell">{{$vertretung->klasse}}</td>
+                                            <td class="d-lg-table-cell">{{$vertretung->stunde}}</td>
+                                            <td class="d-lg-table-cell">{{$vertretung->altFach}} @if($vertretung->neuFach)
+                                                    -> {{$vertretung->neuFach}}@endif</td>
+                                            <td class="d-none d-lg-table-cell">{{$vertretung->lehrer}}</td>
+                                            <td class="d-none d-lg-table-cell">{{$vertretung->comment}}</td>
+                                        </tr>
+                                        <tr class="d-lg-none @if(($loop->iteration-1)%2 == 0) bg-secondary text-white @endif">
+                                            <td class="d-lg-table-cell">{{$vertretung->lehrer}}</td>
+                                            <td class="d-lg-table-cell" colspan="2">{{$vertretung->comment}}</td>
+                                        </tr>
+                                    @endforeach
+                                    <tr class="">
+
+                                    </tr>
+                                    @foreach($mitteilungen->filter(function ($mitteilungen) use ($x) {
+                                        if ((\Carbon\Carbon::make($mitteilungen->date_start)->eq($x) and \Carbon\Carbon::make($mitteilungen->date_end) == null) or (\Carbon\Carbon::make($mitteilungen->date_start)->lessThanOrEqualTo($x) and \Carbon\Carbon::make($mitteilungen->date_end) != null and \Carbon\Carbon::make($mitteilungen->date_end)->greaterThanOrEqualTo($x))){
+                                            return $mitteilungen;
+                                        }
+                                    }) as $dailyNews)
+                                        <tr>
+                                            <th colspan="6" class="border-outline-info">
+                                                {{$dailyNews->news}}
+                                            </th>
+                                        </tr>
+                                    @endforeach
+                                    @if(!is_null($absences))
+                                        <tr>
+                                            <th colspan="6">
+                                                @if($absences->count() > 1)
+                                                    Es fehlen:
+                                                @else
+                                                    Es fehlt:
+                                                @endif
+                                                @foreach($absences->filter(function ($absence) use ($x) {
+                                                    if (Carbon\Carbon::make($absence->start)->lte($x) and Carbon\Carbon::make($absence->end)->gte($x)){
+                                                        return $absence;
+                                                    }
+                                                }) as $absence)
+                                                    {{$absence->user}}@if(!$loop->last),@endif
+                                                @endforeach
+                                            </th>
+                                        </tr>
+                                    @endif
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+        @endfor
+    </div>
+
+    <div class="content d-block d-lg-none">
+        @include('vertretungsplan.vertretungMobil')
     </div>
 @endsection
