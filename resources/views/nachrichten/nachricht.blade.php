4e8c2cd4de883b59f32e5acd80137317af81d7db
Dreknor <Dreknor@users.noreply.github.com>
2022-04-29 16:00:01 +0200

    add Image-Posts

--- resources/views/nachrichten/nachricht.blade.php
+++ resources/views/nachrichten/nachricht.blade.php
@@ -38,42 +38,42 @@
                                 <i class="fas fa-thumbtack"
                                    @if($nachricht->sticky)  style="transform: rotate(45deg)" @endif></i>
                             </a>
-                        @endif
+                            @endif
                     </div>
                 @endif
-
             </div>
-
         </div>
-
         <div class="row mb-2">
-            <div id="carousel_post_{{$nachricht->id}}" class="carousel slide mx-auto" data-ride="carousel">
-                <div class="carousel-inner">
-                    @foreach($nachricht->getMedia('images')->sortBy('name') as $media)
-                        <div class="carousel-item text-center @if($loop->first) active @endif">
-                            <a href="{{url('/image/'.$media->id)}}" target="_blank">
-                                <img class="d-block mx-auto" src="{{url('/image/'.$media->id)}}">
-                                @if(optional($nachricht->rueckmeldung)->type == 'bild')
-                                    <h6 class="small">{{$media->name}}</h6>
-                                @endif
-                            </a>
-                        </div>
-                    @endforeach
+            <div class="col-12">
+                <div id="carousel_post_{{$nachricht->id}}" class="carousel slide mx-auto" data-ride="carousel">
+                    <div class="carousel-inner">
+                        @foreach($nachricht->getMedia('images')->sortBy('name') as $media)
+                            <div class="carousel-item text-center @if($loop->first) active @endif">
+                                <a href="{{url('/image/'.$media->id)}}" target="_blank">
+                                    <img class="d-block mx-auto" src="{{url('/image/'.$media->id)}}">
+                                    @if(optional($nachricht->rueckmeldung)->type == 'bild')
+                                        <h6 class="small">{{$media->name}}</h6>
+                                    @endif
+                                </a>
+                            </div>
+                        @endforeach
 
+                    </div>
+
+                    @if(count($nachricht->getMedia('images'))>1)
+                        <a class="carousel-control-prev" href="#carousel_post_{{$nachricht->id}}" role="button"
+                           data-slide="prev">
+                            <span class="carousel-control-prev-icon bg-primary " aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carousel_post_{{$nachricht->id}}" role="button"
+                           data-slide="next">
+                            <span class="carousel-control-next-icon bg-primary" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    @endif
                 </div>
 
-                @if(count($nachricht->getMedia('images'))>1)
-                    <a class="carousel-control-prev" href="#carousel_post_{{$nachricht->id}}" role="button"
-                       data-slide="prev">
-                        <span class="carousel-control-prev-icon bg-primary " aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carousel_post_{{$nachricht->id}}" role="button"
-                       data-slide="next">
-                        <span class="carousel-control-next-icon bg-primary" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                @endif
             </div>
         </div>
     </div>
