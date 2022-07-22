eac85fcbe6adeee25986eb3deab01f57c59a4630
Dreknor <Dreknor@users.noreply.github.com>
2022-07-22 13:19:47 +0200

    Fehlerbehebung

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -31,15 +31,16 @@ class ImportController extends Controller
                 group_user::truncate();
 
                 $header = [
-                    'klassenstufe' => $request->input('klassenstufe') - 1,
-                    'lerngruppe' => $request->input('lerngruppe') - 1,
-                    'S1Vorname' => $request->input('S1Vorname') - 1,
-                    'S1Nachname' => $request->input('S1Nachname') - 1,
-                    'S1Email' => $request->input('S1Email') - 1,
-                    'S2Email' => $request->input('S2Email') - 1,
-                    'S2Vorname' => $request->input('S2Vorname') - 1,
-                    'S2Nachname' => $request->input('S2Nachname') - 1,
+                    'klassenstufe' => $request->klassenstufe - 1,
+                    'lerngruppe' => $request->lerngruppe - 1,
+                    'S1Vorname' => $request->S1Vorname - 1,
+                    'S1Nachname' => $request->S1Nachname - 1,
+                    'S1Email' => $request->S1Email - 1,
+                    'S2Email' => $request->S2Email - 1,
+                    'S2Vorname' => $request->S2Vorname - 1,
+                    'S2Nachname' => $request->S2Nachname - 1,
                 ];
+
 
                 Excel::import(new UsersImport($header), $request->file('file'));
 
