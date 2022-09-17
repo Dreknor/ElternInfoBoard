7eebeb266893c02dfab411eaeee11f9fa68028b2
Dreknor <Dreknor@users.noreply.github.com>
2022-09-17 14:03:28 +0200

    edit reinigungsansicht

--- resources/views/reinigung/show.blade.php
+++ resources/views/reinigung/show.blade.php
@@ -21,11 +21,13 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <div class="pull-right small">
-                                <a href="{{url('reinigung/'.$Bereich.'/export')}}" class="btn btn-sm">
-                                    Export
-                                </a>
-                            </div>
+                            @can('edit reinigung')
+                                <div class="pull-right small">
+                                    <a href="{{url('reinigung/'.$Bereich.'/export')}}" class="btn btn-sm">
+                                        Export
+                                    </a>
+                                </div>
+                            @endcan
                             <h5 class="card-title">
                                 Reinigungsplan {{$Bereich}}
 
@@ -34,19 +36,21 @@
                         <div class="card-body">
                             <table class="table w-100 table-responsive-sm">
                                 <thead>
-                                    <tr>
-                                        <th>Woche</th>
-                                        <th></th>
-
-                                    </tr>
+                                <tr class="d-none d-sm-block">
+                                    <th>Woche</th>
+                                    <th class="d-none d-sm-block"></th>
+                                </tr>
+                                <tr class="d-block d-sm-none">
+                                    <th class="w-100">Woche</th>
+                                </tr>
                                 </thead>
                                 <tbody>
-                                    @for($Woche = $datum->copy(); $Woche->lessThanOrEqualTo($ende); $Woche->addWeek())
-                                        <tr>
-                                            <th>
-                                                <div class="card bg-light ">
-                                                    <div class="card-header">
-                                                        {{$Woche->copy()->startOfWeek()->format('d.m.')}} - {{$Woche->copy()->endOfWeek()->format('d.m.Y')}}
+                                @for($Woche = $datum->copy(); $Woche->lessThanOrEqualTo($ende); $Woche->addWeek())
+                                    <tr class="d-none d-sm-block">
+                                        <th>
+                                            <div class="card bg-light ">
+                                                <div class="card-header">
+                                                    {{$Woche->copy()->startOfWeek()->format('d.m.')}} - {{$Woche->copy()->endOfWeek()->format('d.m.Y')}}
                                                     </div>
                                                     @can('edit reinigung')
                                                         <div class="card-body">
@@ -57,39 +61,94 @@
                                                     @endcan
                                                 </div>
                                             </th>
-                                            <td>
-                                                <div class="row">
-                                                    @foreach($Familien[$Bereich]->filter(function ($value) use ($Woche){
-                                                        if ($Woche->startOfWeek()->eq($value->datum->startOfWeek())) {return $value;}
-                                                    }) as $reinigung)
-                                                        <div class="col">
-                                                            <div class="card @if($reinigung->user->id == auth()->id() or auth()->user()->sorg2 == auth()->id()) bg-warning @else bg-light @endif">
-                                                                <div class="card-header">
-                                                                   <h6>
-                                                                       Familie {{$reinigung->user->familie_name}}
-                                                                       <div class="pull-right">
-                                                                           <a class="link text-danger" href="{{url('reinigung/'.$Bereich.'/'.$reinigung->id.'/trash')}}">
-                                                                               <i class="fa fa-trash"></i>
-                                                                           </a>
-                                                                       </div>
-                                                                   </h6>
-                                                                </div>
-                                                                <div class="card-body">
-                                                                    {{$reinigung->aufgabe}}
-                                                                </div>
-                                                                <div class="card-footer">
-                                                                    {{$reinigung->bemerkung}}
-                                                                </div>
+                                        <td>
+                                            <div class="row">
+                                                @foreach($Familien[$Bereich]->filter(function ($value) use ($Woche){
+                                                    if ($Woche->startOfWeek()->eq($value->datum->startOfWeek())) {return $value;}
+                                                }) as $reinigung)
+                                                    <div class="col">
+                                                        <div
+                                                            class="card @if($reinigung->user->id == auth()->id() or auth()->user()->sorg2 == auth()->id()) bg-warning @else bg-light @endif">
+                                                            <div class="card-header">
+                                                                <h6>
+                                                                    Familie {{$reinigung->user->familie_name}}
+                                                                    @can('edit reinigung')
+                                                                        <div class="pull-right">
+                                                                            <a class="link text-danger"
+                                                                               href="{{url('reinigung/'.$Bereich.'/'.$reinigung->id.'/trash')}}">
+                                                                                <i class="fa fa-trash"></i>
+                                                                            </a>
+                                                                        </div>
+                                                                    @endcan
+                                                                </h6>
+                                                            </div>
+                                                            <div class="card-body">
+                                                                {{$reinigung->aufgabe}}
+                                                            </div>
+                                                            <div class="card-footer">
+                                                                {{$reinigung->bemerkung}}
                                                             </div>
                                                         </div>
+                                                    </div>
 
-                                                    @endforeach
+                                                @endforeach
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr class="d-block d-sm-none">
+                                        <th class="w-100">
+                                            <div class="card bg-light ">
+                                                <div class="card-header">
+                                                    {{$Woche->copy()->startOfWeek()->format('d.m.')}}
+                                                    - {{$Woche->copy()->endOfWeek()->format('d.m.Y')}}
                                                 </div>
+                                                @can('edit reinigung')
+                                                    <div class="card-body">
+                                                        <a href="{{url('reinigung/create/'.$Bereich.'/'.$Woche->startOfWeek()->format('Ymd'))}}"
+                                                           class="btn btn-sm">
+                                                            <i class="fas fa-plus-circle"></i>
+                                                        </a>
+                                                    </div>
+                                                @endcan
+                                            </div>
+                                        </th>
+                                    </tr>
+                                    <tr class="d-block d-sm-none">
+                                        <td class="w-100">
+                                            <div class="row">
+                                                @foreach($Familien[$Bereich]->filter(function ($value) use ($Woche){
+                                                    if ($Woche->startOfWeek()->eq($value->datum->startOfWeek())) {return $value;}
+                                                }) as $reinigung)
+                                                    <div class="col">
+                                                        <div
+                                                            class="card @if($reinigung->user->id == auth()->id() or auth()->user()->sorg2 == auth()->id()) bg-warning @else bg-light @endif">
+                                                            <div class="card-header">
+                                                                <h6>
+                                                                    Familie {{$reinigung->user->familie_name}}
+                                                                    @can('edit reinigung')
+                                                                        <div class="pull-right">
+                                                                            <a class="link text-danger"
+                                                                               href="{{url('reinigung/'.$Bereich.'/'.$reinigung->id.'/trash')}}">
+                                                                                <i class="fa fa-trash"></i>
+                                                                            </a>
+                                                                        </div>
+                                                                    @endcan
+                                                                </h6>
+                                                            </div>
+                                                            <div class="card-body">
+                                                                {{$reinigung->aufgabe}}
+                                                            </div>
+                                                            <div class="card-footer">
+                                                                {{$reinigung->bemerkung}}
+                                                            </div>
+                                                        </div>
+                                                    </div>
 
-                                            </td>
-
-                                        </tr>
-                                    @endfor
+                                                @endforeach
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endfor
                                 </tbody>
                             </table>
                         </div>
