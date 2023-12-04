5cb6e2e1415b0eda13b58d0eafc01fc378e1b766
Dreknor <Dreknor@users.noreply.github.com>
2023-12-04 13:40:43 +0100

    Bilder von Nachrichten -> Anzeigeform ändern

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -34,10 +34,11 @@ class ImageController extends Controller
         return $response;
     }
 
-    public function changeCollection(Media $media_id, string $collection_name)
+    public function changeCollection(Media $media, string $collection_name)
     {
-        $media_id->collection_name = $collection_name;
-        $media_id->save();
+        $media->update([
+            'collection_name' => $collection_name,
+        ]);
         return redirect()->back()->with([
             'type' => 'success',
             'message' => 'Datei in ' . $collection_name . ' verschoben',
