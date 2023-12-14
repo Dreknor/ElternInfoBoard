fe38049ec44bee5f365377e803c983b1e5bdb711
Dreknor <Dreknor@users.noreply.github.com>
2023-12-14 14:39:34 +0100

    Start API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,13 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
+use Symfony\Component\{HttpKernel\Exception\HttpException,
+    HttpKernel\Exception\NotFoundHttpException,
+    Routing\Exception\MethodNotAllowedException};
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,21 +42,37 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
-        if (app()->bound('sentry') && $this->shouldReport($exception)) {
-            app('sentry')->captureException($exception);
+        if (app()->bound('sentry') && $this->shouldReport($exception) && app()->environment('production')) {
+            //app('sentry')->captureException($exception);
         }
+
+        Log::info($exception->getMessage());
+        Log::info(Request::fullUrl());
 
         parent::report($exception);
     }
 
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof ModelNotFoundException || $exception instanceof NotFoundHttpException) {
+        $url = Request::fullUrl();
+
+        Log::info("404 in {$url}");
+        }
+
+
+        if ($exception instanceof MethodNotAllowedException) {
+            Log::info("405 in {$url}");
+        }
+
         // Convert all non-http exceptions to a proper 500 http exception
         // if we don't do this exceptions are shown as a default template
         // instead of our own view in resources/views/errors/500.blade.php
         if ($this->shouldReport($exception) && ! $this->isHttpException($exception) && ! config('app.debug')) {
             $exception = new HttpException(500, 'Whoops!');
         }
+
+
 
         return parent::render($request, $exception);
     }
