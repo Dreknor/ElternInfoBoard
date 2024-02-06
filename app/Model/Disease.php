dee600dcfe6dbdff5dc4e74e061cf1afa3886842
Dreknor <Dreknor@users.noreply.github.com>
2024-02-06 06:14:06 +0100

    extend existing disease

--- app/Model/Disease.php
+++ app/Model/Disease.php
@@ -4,6 +4,7 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Disease extends Model
 {
