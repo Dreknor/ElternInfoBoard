0cf61126e028f60368d7e387f2d0dddf38427d94
Shift <shift@laravelshift.com>
2024-03-25 12:30:12 +0000

    Add custom Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+"content":["app/**/*.php","resources/**/*.js","resources/**/*.php","resources/**/*.vue"],"theme":{
+"extend":{
+"colors":{
+"green":colors.green}}},"plugins":[]}
