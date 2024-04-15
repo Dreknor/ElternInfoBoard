8193ac3b1945d7dbcc173a734eafefebe3527de7
Dreknor <Dreknor@users.noreply.github.com>
2024-04-15 17:32:46 +0200

    Log Vertretungsplan Notifications

--- app/Observers/VertretungObserver.php
+++ app/Observers/VertretungObserver.php
@@ -17,21 +17,25 @@ class VertretungObserver
 
         Log::info('Vertretung created: ' . $vertretung->id . ' for ' . $group->name . ' on ' . $vertretung->date . ' in the ' . $vertretung->stunde . ' hour.');
         Log::info('Sending notification to ' . $users->count() . ' users.');
+
+        $notifications = [];
+
         foreach ($users as $user) {
-            $notification = new Notification([
+            $notifications[]= array(
                 'title' => 'Vertretung',
                 'url' => '/vertretungsplan/',
                 'type' => 'vertretung',
-                'message' => 'Änderung im Vertretungsplan für ' . $group->name . ' am ' . Carbon::createFromFormat('Y-m-d', $vertretung->date)->format('d.m.Y') . ' in der ' . $vertretung->stunde . ' Stunde.'
-
-            ]);
-            $user->notifications()->save($notification);
+                'message' => 'Änderung im Vertretungsplan für ' . $group->name . ' am ' . Carbon::createFromFormat('Y-m-d', $vertretung->date)->format('d.m.Y') . ' in der ' . $vertretung->stunde . ' Stunde.',
+                'user_id' => $user->id,
+            );
 
             if ($user->webPushSubscriptions->count() > 0 and $user->can('testing')) {
                 Log::info('Sending notification to ' . $user->name);
                 $user->notify(new VertretungsplanNotification($notification->title, $notification->message));
             }
         }
+
+        Notification::insert($notifications);
     }
 
 
