360a15b4ec1d89bc4740a46b849b7d435cabee07
Dreknor <Dreknor@users.noreply.github.com>
2024-08-05 13:49:38 +0200

    Fehlerbehebung

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -7,6 +7,7 @@ use App\Model\User;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
@@ -57,52 +58,60 @@ class UsersImport implements ToCollection, WithHeadingRow
                 $email1 = explode(';', $row[$this->header['S1Email']]);
                 $email1 = $email1[0];
 
-                $user1 = User::firstOrCreate([
-                    'email' => $email1,
-                ],
-                    [
-                        'name' => $row[$this->header['S1Vorname']].' '.$row[$this->header['S1Nachname']],
-                        'changePassword' => 1,
-                        'password' => Hash::make(config('import_eltern')),
-                        'lastEmail' => Carbon::now(),
-                    ]);
+                try {
+                    $user1 = User::firstOrCreate([
+                        'email' => $email1,
+                    ],
+                        [
+                            'name' => $row[$this->header['S1Vorname']] . ' ' . $row[$this->header['S1Nachname']],
+                            'changePassword' => 1,
+                            'password' => Hash::make(config('import_eltern')),
+                            'lastEmail' => Carbon::now(),
+                        ]);
 
-                $user1->touch();
-                $user1->assignRole('Eltern');
-                $user1->removeRole('Aufnahme');
+                    $user1->touch();
+                    $user1->assignRole('Eltern');
+                    $user1->removeRole('Aufnahme');
 
-                $user1->groups()->attach($gruppen);
+                    $user1->groups()->attach($gruppen);
+
+                    if (!is_null($row[$this->header['S2Email']])) {
+                        $email2 = explode(';', $row[$this->header['S2Email']]);
+                        $email2 = $email2[0];
+
+                        $user2 = User::firstOrCreate([
+                            'email' => $email2,
+                        ],
+                            [
+                                'name' => $row[$this->header['S2Vorname']] . ' ' . $row[$this->header['S2Nachname']],
+                                'changePassword' => 1,
+                                'password' => Hash::make(config('import_eltern')),
+                                'lastEmail' => Carbon::now(),
+                            ]);
+
+                        $user2->touch();
+                        $user2->assignRole('Eltern');
+                        $user2->removeRole('Aufnahme');
+                        $user2->groups()->attach($gruppen);
+
+                    }
+
+                    if (isset($user2) and isset($user1) and $user2->id != $user1->id and isset($user2->email) and isset($user1->email)) {
+                        $user2->sorg2 = $user1->id;
+                        $user1->sorg2 = $user2->id;
+
+                        $user2->save();
+                        $user1->save();
+                    }
+                } catch (\Exception $e) {
+                    Log::error('Fehler beim Importieren von ' . $row[$this->header['S1Vorname']] . ' ' . $row[$this->header['S1Nachname']]);
+                    Log::error($e->getMessage());
+                }
+
 
             }
 
-            if (! is_null($row[$this->header['S2Email']])) {
-                $email2 = explode(';', $row[$this->header['S2Email']]);
-                $email2 = $email2[0];
 
-                $user2 = User::firstOrCreate([
-                    'email' => $email2,
-                ],
-                    [
-                        'name' => $row[$this->header['S2Vorname']].' '.$row[$this->header['S2Nachname']],
-                        'changePassword' => 1,
-                        'password' => Hash::make(config('import_eltern')),
-                        'lastEmail' => Carbon::now(),
-                    ]);
-
-                $user2->touch();
-                $user2->assignRole('Eltern');
-                $user2->removeRole('Aufnahme');
-                $user2->groups()->attach($gruppen);
-
-            }
-
-            if (isset($user2) and isset($user1) and $user2->id != $user1->id and isset($user2->email) and isset($user1->email)) {
-                $user2->sorg2 = $user1->id;
-                $user1->sorg2 = $user2->id;
-
-                $user2->save();
-                $user1->save();
-            }
         }
     }
 }
