e90f4f5333494b030de665c05a027dd08d21213b
Dreknor <Dreknor@users.noreply.github.com>
2024-08-06 17:47:57 +0200

    Fehlerbehebung Rückmeldung

--- app/Http/Controllers/ICalController.php
+++ app/Http/Controllers/ICalController.php
@@ -17,73 +17,78 @@ class ICalController extends Controller
      */
     public function createICal($uuid)
     {
-        $user = User::where('uuid', $uuid)->firstOrFail();
-        $prefix = ($user->calendar_prefix != null) ? '(' . $user->calendar_prefix . ') ' : '';
+        try {
+            $user = User::where('uuid', $uuid)->firstOrFail();
+            $prefix = ($user->calendar_prefix != null) ? '(' . $user->calendar_prefix . ') ' : '';
 
-        if ($user->releaseCalendar) {
-            $Termine = $user->termine;
+            if ($user->releaseCalendar) {
+                $Termine = $user->termine;
 
-            //Termine aus Listen holen
-            $listen_termine = $user->listen_termine()->whereDate('termin', '>', Carbon::now()->startOfDay())->get();
+                //Termine aus Listen holen
+                $listen_termine = $user->listen_termine()->whereDate('termin', '>', Carbon::now()->startOfDay())->get();
 
-            //Ergänze Listeneintragungen
-            if (! is_null($listen_termine) and count($listen_termine) > 0) {
-                foreach ($listen_termine as $termin) {
-                    $newTermin = new Termin([
-                        'terminname' => $prefix . '' . $termin->liste->listenname,
-                        'start' => $termin->termin->timezone('Europe/Berlin'),
-                        'ende' => $termin->termin->copy()->addMinutes($termin->liste->duration),
-                        'fullDay' => null,
-                    ]);
-                    $Termine->push($newTermin);
+                //Ergänze Listeneintragungen
+                if (! is_null($listen_termine) and count($listen_termine) > 0) {
+                    foreach ($listen_termine as $termin) {
+                        $newTermin = new Termin([
+                            'terminname' => $prefix . '' . $termin->liste->listenname,
+                            'start' => $termin->termin->timezone('Europe/Berlin'),
+                            'ende' => $termin->termin->copy()->addMinutes($termin->liste->duration),
+                            'fullDay' => null,
+                        ]);
+                        $Termine->push($newTermin);
+                    }
                 }
-            }
 
-            //Listentermine von Sorg2
-            if (! is_null($user->sorgeberechtigter2)) {
-                foreach ($user->sorgeberechtigter2->listen_termine()->whereDate('termin', '>', Carbon::now()->startOfDay())->get() as $termin) {
-                    $newTermin = new Termin([
-                        'terminname' => $prefix . '' . $termin->liste->listenname,
-                        'start' => $termin->termin->timezone('Europe/Berlin'),
-                        'ende' => $termin->termin->copy()->addMinutes($termin->liste->duration),
-                        'fullDay' => null,
-                    ]);
-                    $Termine->push($newTermin);
+                //Listentermine von Sorg2
+                if (! is_null($user->sorgeberechtigter2)) {
+                    foreach ($user->sorgeberechtigter2->listen_termine()->whereDate('termin', '>', Carbon::now()->startOfDay())->get() as $termin) {
+                        $newTermin = new Termin([
+                            'terminname' => $prefix . '' . $termin->liste->listenname,
+                            'start' => $termin->termin->timezone('Europe/Berlin'),
+                            'ende' => $termin->termin->copy()->addMinutes($termin->liste->duration),
+                            'fullDay' => null,
+                        ]);
+                        $Termine->push($newTermin);
+                    }
                 }
-            }
 
-            $Termine = $Termine->unique('id');
-            $Termine = $Termine->sortBy('start');
+                $Termine = $Termine->unique('id');
+                $Termine = $Termine->sortBy('start');
 
-            $icalObject = Calendar::create(config('app.name'));
+                $icalObject = Calendar::create(config('app.name'));
 
-            // loop over events
-            foreach ($Termine as $event) {
-                if ($event->fullDay) {
-                    $icalObject->event(Event::create()
-                        ->name($prefix . '' . $event->terminname)
-                        ->uniqueIdentifier(($event->id) ?: uuid_create())
-                        ->startsAt($event->start->timezone('Europe/Berlin'))
-                        ->endsAt($event->ende->timezone('Europe/Berlin'))
-                        ->withoutTimezone()
-                        ->fullDay());
-                } else {
-                    $icalObject->event(Event::create()
-                        ->name($prefix . '' . $event->terminname)
-                        ->startsAt($event->start->timezone('Europe/Berlin'))
-                        ->endsAt($event->ende->timezone('Europe/Berlin'))
-                        ->uniqueIdentifier(($event->id) ?: uuid_create())
-                    );
+                // loop over events
+                foreach ($Termine as $event) {
+                    if ($event->fullDay) {
+                        $icalObject->event(Event::create()
+                            ->name($prefix . '' . $event->terminname)
+                            ->uniqueIdentifier(($event->id) ?: uuid_create())
+                            ->startsAt($event->start->timezone('Europe/Berlin'))
+                            ->endsAt($event->ende->timezone('Europe/Berlin'))
+                            ->withoutTimezone()
+                            ->fullDay());
+                    } else {
+                        $icalObject->event(Event::create()
+                            ->name($prefix . '' . $event->terminname)
+                            ->startsAt($event->start->timezone('Europe/Berlin'))
+                            ->endsAt($event->ende->timezone('Europe/Berlin'))
+                            ->uniqueIdentifier(($event->id) ?: uuid_create())
+                        );
+                    }
                 }
-            }
 
-            return response($icalObject->get(), 200, [
-                'Content-Type' => 'text/calendar; charset=utf-8',
-                'Content-Disposition' => 'attachment; filename="'.config('app.name').'.ics"',
-            ]);
-        } else {
+                return response($icalObject->get(), 200, [
+                    'Content-Type' => 'text/calendar; charset=utf-8',
+                    'Content-Disposition' => 'attachment; filename="'.config('app.name').'.ics"',
+                ]);
+            } else {
+                abort(404);
+            }
+        } catch (\Exception $e) {
             abort(404);
         }
+
     }
 
     /**
