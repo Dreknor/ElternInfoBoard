f9e7bb60fd6c7916c716574a9d47f40faaa89d57
Dreknor <Dreknor@users.noreply.github.com>
2024-09-12 19:48:34 +0200

    Anpassung Listeneintragung

--- app/Http/Controllers/ListenEintragungenController.php
+++ app/Http/Controllers/ListenEintragungenController.php
@@ -93,6 +93,7 @@ class ListenEintragungenController extends Controller
                 'title' => 'Eintragung '.$listen_eintragung->eintragung.' wurde gelöscht',
                 'message' => 'Eintragung wurde von ' . auth()->user()->name . ' in der Liste ' . $listen_eintragung->liste->listenname . ' entfernt',
                 'icon' => 'https://eltern.esz-radebeul.de/img/favicon-esz.ico',
+                'url' => url('listen/'.$listen_eintragung->listen_id),
             ]);
             $notification->save();
 
