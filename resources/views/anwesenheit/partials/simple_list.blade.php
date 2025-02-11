9f4b85379c9b67ca696a3d8d5e495309f97d2aaa
Dreknor <Dreknor@users.noreply.github.com>
2025-02-11 11:02:14 +0100

    Dev Care-Modul

--- resources/views/anwesenheit/partials/simple_list.blade.php
+++ resources/views/anwesenheit/partials/simple_list.blade.php
@@ -2,12 +2,14 @@
     @foreach($groups as $group)
         <div class="col-lg-3 col-md-6 mb-4">
             <div class="card">
-                <div class="card-header bg-primary text-white">
+                <div class="card-header bg-primary text-white" style="position: sticky; top: 0; z-index: 1;">
                     <h3>{{ $group }}</h3>
                 </div>
                 <div class="card-body">
                     @foreach($classes as $class)
-                        <h4 class="text-secondary">{{ $class }}</h4>
+                        <h4 class="text-secondary" style="position: sticky; top: 40px; z-index: 1; background-color: white;">
+                            {{ $class }}
+                        </h4>
                         @php
                             $sortedChildren = $children->where('group', $group)->where('class', $class)->sortBy('lastname');
                         @endphp
