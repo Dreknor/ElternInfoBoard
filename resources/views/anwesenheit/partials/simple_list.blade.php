ac38eb767c198da1465e0d8ae3691d0538e616a3
Dreknor <Dreknor@users.noreply.github.com>
2025-02-27 15:37:41 +0100

    Refactor care settings usage in simple_list.blade.php for improved readability and maintainability

--- resources/views/anwesenheit/partials/simple_list.blade.php
+++ resources/views/anwesenheit/partials/simple_list.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
     <div class="row">
         @foreach($groups as $group)
-            @if((new \App\Settings\CareSetting())->hide_groups_when_empty and $children->where('group_id', $group->id)->count() == 0)
+            @if($careSettings->hide_groups_when_empty and $children->where('group_id', $group->id)->count() == 0)
                 @continue
             @endif
             <div class="col-lg-3 col-md-6 mb-1">
@@ -14,7 +14,7 @@
                     </div>
                     <div class="card-body" style="padding: 0.5rem;">
                         @foreach($classes as $class)
-                            @if((new \App\Settings\CareSetting())->hide_groups_when_empty and $children->where('group_id', $group->id)->where('class_id', $class->id)->count() == 0)
+                            @if($careSettings->hide_groups_when_empty and $children->where('group_id', $group->id)->where('class_id', $class->id)->count() == 0)
                                 @continue
                             @endif
                             <h4 class="bg-gradient-directional-grey-blue text-white p-2" style="position: sticky; top: 60px; z-index: 1; margin: 0.5rem 0;">
@@ -32,11 +32,21 @@
                                         <div class="container-fluid">
                                             <div class="row">
                                                 <div class="col-2 d-flex justify-content-center align-items-center">
+                                                    @if($child->should_be_today() and !$child->checkedIn())
+                                                        <div class="bg-info text-white rounded-circle" style="width: 25px; height: 25px; display: flex; justify-content: center; align-items: center;">
+                                                            <i class="fas fa-question"></i>
+                                                        </div>
+                                                    @else
+
+                                                    @endif
+
+
                                                     @if($child->hasNotice())
                                                         <div class="bg-info text-white rounded-circle" style="width: 25px; height: 25px; display: flex; justify-content: center; align-items: center;">
                                                             <i class="fas fa-envelope"></i>
                                                         </div>
                                                     @endif
+
 
                                                     @if($child->krankmeldungToday())
                                                         <div class="badge badge-danger">
@@ -45,7 +55,7 @@
                                                     @endif
 
                                                 </div>
-                                                <div class="col-auto d-flex justify-content-center align-items-center">
+                                                <div class="col-auto d-flex justify-content-center align-items-center name">
                                                         {{ $child->last_name }}, {{ $child->first_name }}
                                                 </div>
                                                 <div class="col">
@@ -59,7 +69,7 @@
                                                                 if($schickzeit->type == 'ab' and $currentTime->isBefore($schickzeit->time_ab)) {
                                                                     $backgroundClass .= 'success';
                                                                     $text_size = 'text-smaller';
-                                                                } elseif($schickzeit->type == 'ab' and $currentTime->isAfter($schickzeit->time_ab) and $currentTime->isBefore($schickzeit->time_spaet)) {
+                                                                } elseif($schickzeit->type == 'ab' and ($schickzeit->time_ab && $currentTime->isAfter($schickzeit->time_ab)) and ($schickzeit->time_spaet && $currentTime->isBefore($schickzeit->time_spaet))) {
                                                                     $backgroundClass .= 'warning';
                                                                     $text_size = 'text-great';
                                                                 } elseif($schickzeit->type == 'ab' and $currentTime->isAfter($schickzeit->time_spaet)) {
@@ -103,7 +113,7 @@
 
                                     </li>
                                     @empty
-                                        @if((new \App\Settings\CareSetting())->show_message_on_empty_group)
+                                        @if($careSettings->show_message_on_empty_group)
                                             <li class="list-group-item bg-gradient-directional-light-yellow" style="padding: 0.5rem;">
                                                 Keine Kinder in dieser Klassenstufe
                                             </li>
