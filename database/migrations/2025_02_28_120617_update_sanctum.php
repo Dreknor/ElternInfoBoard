f0ad4121607e961349121caf031b26f0a5085187
Dreknor <pauli84@freenet.de>
2025-03-28 10:50:39 +0100

    Add checks to prevent duplicate migrations for personal_access_tokens and failed_jobs tables

--- database/migrations/2025_02_28_120617_update_sanctum.php
+++ database/migrations/2025_02_28_120617_update_sanctum.php
@@ -11,9 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('personal_access_tokens', function (Blueprint $table) {
-            $table->timestamp('expires_at')->nullable()->after('last_used_at');
-        });
+
+        if (!Schema::hasColumn('personal_access_tokens', 'expires_at')){
+            Schema::table('personal_access_tokens', function (Blueprint $table) {
+                $table->timestamp('expires_at')->nullable()->after('last_used_at');
+            });
+        }
+
+
     }
 
     /**
