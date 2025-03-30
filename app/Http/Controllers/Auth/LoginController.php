1295deb75d935e44e12a2d1311071d6936ca4d33
Dreknor <Dreknor@users.noreply.github.com>
2025-03-30 13:44:10 +0200

    Refactor password-less login method to improve logging and return type

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -56,23 +56,18 @@ class LoginController extends Controller
 
         if ($request->input('submit') == 'password-less') {
 
-            Log::info('password-less-login: ');
-            Log::info($request->input('email'));
 
             $user = $this->loginViaPasswordLessLink($request);
 
             if (!$user or !$user->can('allow password-less-login')) {
 
-                Log::info('password-less-login: ');
-                Log::info('Benutzer existiert nicht oder hat keine Berechtigung für den Passwortlosen Login.');
+
 
                 return redirect()->route('login')
                     ->withErrors(['email' => 'Benutzer existiert nicht oder hat keine Berechtigung für den Passwortlosen Login.'])
                     ->withInput();
             }
 
-            Log::info('password-less-login: ');
-            Log::info('Login-Link wurde an die angegebene E-Mail-Adresse gesendet.');
 
             return redirect()->route('login')
                 ->with([
@@ -97,8 +92,6 @@ class LoginController extends Controller
             return $this->sendLoginResponse($request);
         }
 
-        Log::info('Login fehlgeschlagen: ');
-        Log::info($request->input('email'));
 
         $this->incrementLoginAttempts($request);
 
@@ -110,12 +103,11 @@ class LoginController extends Controller
      * @param Request $request
      * @return User
      */
-    public function loginViaPasswordLessLink(Request $request): User
+    public function loginViaPasswordLessLink(Request $request): ?User
     {
 
 
         $user = User::where('email', $request->input('email'))->first();
-
 
 
         if ($user and $user->can('allow password-less-login')) {
@@ -127,9 +119,11 @@ class LoginController extends Controller
 
 
             $user->notify(new SendPasswordLessLinkNotification($url));
+
+            return $user;
         }
 
-        return $user;
+        return null;
     }
 
     /**
