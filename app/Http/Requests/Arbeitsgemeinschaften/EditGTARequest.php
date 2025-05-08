81abd7fb854907f7de8e04482206929180606e56
Dreknor <Dreknor@users.noreply.github.com>
2025-05-08 11:05:05 +0200

    Make description field nullable in CreateGTARequest and EditGTARequest

--- app/Http/Requests/Arbeitsgemeinschaften/EditGTARequest.php
+++ app/Http/Requests/Arbeitsgemeinschaften/EditGTARequest.php
@@ -23,7 +23,7 @@ class EditGTARequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'description' => 'required|string',
+            'description' => 'nullable|string',
             'weekday' => 'required|string',
             'start_time' => 'required|date_format:H:i',
             'end_time' => 'required|date_format:H:i|after:start_time',
