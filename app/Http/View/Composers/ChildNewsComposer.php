ef6f822639432886786336cb5eb6f90707dc343e
Dreknor <Dreknor@users.noreply.github.com>
2025-07-04 09:41:26 +0200

    Add Schickzeiten module and update child card view for attendance and notices

---
+++ app/Http/View/Composers/ChildNewsComposer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\View\Composers;
+
+use App\Settings\CareSetting;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+
+class ChildNewsComposer
+{
+    public function compose($view): void
+    {
+        $children = auth()->user()->children();
+
+        if ($children->count() > 0) {
+            $allowedClasses = Cache::remember('careSettings_classes', now()->addDay(), function ()  use ($children){
+                $careSettings = new CareSetting();
+                return $careSettings->class_list;
+            });
+
+            $allowedGroups = Cache::remember('careSettings_groups', now()->addDay(), function () use ($children) {
+                $careSettings = new CareSetting();
+                return $careSettings->groups_list;
+            });
+
+            $children = $children->filter(function ($child) use ($allowedClasses, $allowedGroups) {
+                return in_array($child->class_id, $allowedClasses) && in_array($child->group_id, $allowedGroups);
+            });
+            $children = $children->load(['checkIns' => fn ($query) => $query->where('date', '>=', today())->where('date', '<=', Carbon::now()->addDays(7))])
+                ->sortBy(function ($child) {
+                    return $child->checkIns->count() > 0 ? $child->checkIns->first()->date : now();
+                });
+
+        }
+
+
+        $view->with('children', $children);
+    }
+}
---
+++ resources/views/child/include/home-header.blade.php
@@ -0,0 +1,85 @@
+@if($children->count() > 0)
+    <div class="container-fluid">
+        <div class="row">
+            @foreach($children as $child)
+                <div class="col-md-4">
+                    <div class="card border">
+                        <div class="@if($child->checkedIn()) bg-gradient-directional-teal @else bg-info @endif card-header border-bottom text-white pt-1">
+                            <h6 class="card-title">
+                                @if($child->krankmeldungToday())
+                                    <div class="badge badge-danger mr-2">
+                                        <i class="fas fa-ban"></i> Krank
+                                    </div>
+                                @endif
+                                {{ $child->first_name }}
+                                    <a href="{{ url('schickzeiten') }}" class="text-white float-right p-1 border mb-1">
+                                        <i class="fas fa-eye"></i>
+                                    </a>
+                            </h6>
+
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    @if(!$child->checkedIn() and $child->checkIns()->where('date', today())->first())
+                                        <p>
+                                            Abgemeldet um
+                                            {{$child->checkIns()->where('date', today())->first()?->updated_at?->format('H:i')}}
+                                            Uhr
+                                        </p>
+                                    @elseif($child->checkedIn())
+                                        <p>
+                                            <i class="fas fa-user-check text-success"></i> derzeit angemeldet
+                                            @if($child->getSchickzeitenForToday()->count() > 0)
+                                                @foreach($child->getSchickzeitenForToday() as $schickzeit)
+                                                    <br><b>Schickzeit:</b>
+                                                    @if($schickzeit->type == 'genau')
+                                                        {{$schickzeit->time?->format('H:i')}} Uhr
+                                                    @else
+                                                        @if(!is_null($schickzeit->time_ab))
+                                                            ab
+                                                        @endif
+                                                        {{$schickzeit->time_ab?->format('H:i')}}
+                                                        @if(!is_null($schickzeit->time_ab) && !is_null($schickzeit->time_spaet))
+                                                            -
+                                                        @endif
+                                                        @if(!is_null($schickzeit->time_spaet))
+                                                            spät.
+                                                        @endif
+                                                        {{$schickzeit->time_spaet?->format('H:i')}} Uhr
+                                                    @endif
+                                                @endforeach
+                                            @else
+                                                <br>keine Schickzeiten
+                                            @endif
+                                            @if($child->hasNotice())
+                                                <br>
+                                                <b>Nachricht:</b> {{$child->hasNotice()->notice}}
+                                            @endif
+                                        </p>
+                                    @else
+                                        heute nicht angemeldet
+                                    @endif
+                                </div>
+                                <div class="col-md-6">
+                                    <b>Anwesenheit:</b>
+                                    <p>
+                                        @forelse($child->checkIns->sortBy('date') as $checkIn)
+                                                {{ $checkIn->date->format('d.m.Y') }} @if($checkIn->should_be) <i class="text-success">angemeldet </i> @else <i class="text-danger">nicht angemeldet </i>  @endif <br>
+                                        @empty
+                                            <tr>
+                                                <td colspan="5">Keine Anwesenheitsabfragen vorhanden</td>
+                                            </tr>
+                                        @endforelse
+                                    </p>
+
+                                </div>
+                            </div>
+
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+@endif
