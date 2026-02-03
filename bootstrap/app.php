c25b21d4f288919b0e03683c41fa2ac6030ccfba
Shift <shift@laravelshift.com>
2026-02-03 18:05:00 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,30 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo('/home');
+
+        $middleware->web([
+            \App\Http\Middleware\CheckNewsForUser::class,
+            \App\Http\Middleware\LastOnlineAt::class,
+        ]);
+
+        $middleware->throttleApi();
+
+        $middleware->alias([
+            'mark_passwordless_login' => \App\Http\Middleware\MarkPasswordlessLogin::class,
+            'password_expired' => \App\Http\Middleware\PasswordExpired::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        ]);
+
+        $middleware->priority([
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\Authenticate::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Auth\Middleware\Authorize::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
