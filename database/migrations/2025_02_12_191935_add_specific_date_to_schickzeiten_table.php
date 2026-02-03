07e2ebb488c12ac896e7bfcf3bfade7453c8ee72
Shift <shift@laravelshift.com>
2026-02-03 14:32:03 +0000

    Adopt anonymous migrations

--- database/migrations/2025_02_12_191935_add_specific_date_to_schickzeiten_table.php
+++ database/migrations/2025_02_12_191935_add_specific_date_to_schickzeiten_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSpecificDateToSchickzeitenTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -20,4 +20,4 @@ class AddSpecificDateToSchickzeitenTable extends Migration
             $table->dropColumn('specific_date');
         });
     }
-}
+};
