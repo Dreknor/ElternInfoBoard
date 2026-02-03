e1614b7fde0df59c43f896c13f01ed1cd446d4ff
Shift <shift@laravelshift.com>
2026-02-03 18:05:01 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,11 +10,15 @@ use App\Observers\UserRueckmeldungenObserver;
 use App\Observers\VertretungObserver;
 use App\Settings\GeneralSetting;
 use Carbon\Carbon;
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -22,6 +26,15 @@ use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to the "home" route for your application.
+     *
+     * This is used by Laravel authentication to redirect users after login.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
     /**
      * Register any application services.
      */
@@ -162,5 +175,23 @@ class AppServiceProvider extends ServiceProvider
                 return $sortBy($this);
             });
         }
+
+        $this->bootRoute();
+    }
+
+    public function bootRoute()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(80)->by($request->user()?->id ?: $request->ip());
+        });
+
+        Route::model('event', \App\Model\ElternratEvent::class);
+        Route::model('task', \App\Model\ElternratTask::class);
+
+
     }
 }
