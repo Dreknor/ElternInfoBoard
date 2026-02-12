cff82603acb9b20fccc5917f305612379f35af9e
Dreknor <Dreknor@users.noreply.github.com>
2026-02-12 06:59:11 +0100

    Refactor ActiveDisease active method to use Builder type hint and update postsNotArchived method to filter archived posts

--- app/Model/ActiveDisease.php
+++ app/Model/ActiveDisease.php
@@ -2,7 +2,6 @@
 
 namespace App\Model;
 
-use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -34,8 +33,7 @@ class ActiveDisease extends Model
         return $this->belongsTo(User::class);
     }
 
-    #[Scope]
-    protected function active(Builder $query): Builder
+    public function scopeActive(Builder $query): Builder
     {
         return $query->where('active', 1);
     }
