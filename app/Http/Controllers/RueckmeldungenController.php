d8286be806b4688c03bfb712c2d7f8b4d2af4324
Dreknor <Dreknor@users.noreply.github.com>
2026-06-04 17:33:19 +0200

    feat: Add soft deletes to feedback tables and update models for soft delete functionality

--- app/Http/Controllers/RueckmeldungenController.php
+++ app/Http/Controllers/RueckmeldungenController.php
@@ -537,6 +537,12 @@ class RueckmeldungenController extends Controller
      */
     public function destroy(Rueckmeldungen $rueckmeldung)
     {
+        Log::info('Rückmeldung gelöscht', [
+            'id' => $rueckmeldung->id,
+            'post_id' => $rueckmeldung->post_id,
+            'type' => $rueckmeldung->type,
+            'deleted_by' => auth()->id(),
+        ]);
         $rueckmeldung->delete();
 
         return response()->json([
@@ -558,6 +564,12 @@ class RueckmeldungenController extends Controller
             ]);
         }
 
+        Log::info('Abfrage gelöscht', [
+            'id' => $rueckmeldung->id,
+            'post_id' => $rueckmeldung->post_id,
+            'deleted_by' => auth()->id(),
+        ]);
+
         $rueckmeldung->options()->delete();
         $rueckmeldung->delete();
 
@@ -574,6 +586,11 @@ class RueckmeldungenController extends Controller
      */
     public function destroyRueckmeldung(Rueckmeldungen $rueckmeldungen)
     {
+        Log::info('Rückmeldung gelöscht (destroyRueckmeldung)', [
+            'id' => $rueckmeldungen->id,
+            'post_id' => $rueckmeldungen->post_id,
+            'deleted_by' => auth()->id(),
+        ]);
         $rueckmeldungen->delete();
 
         return redirect()->back()->with([
--- app/Model/UserRueckmeldungen.php
+++ app/Model/UserRueckmeldungen.php
@@ -8,11 +8,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[ObservedBy([UserRueckmeldungenObserver::class])]
 class UserRueckmeldungen extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'users_rueckmeldungen';
 
