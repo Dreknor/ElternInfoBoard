5f4803f52649830cd687b29490ac0d9922309386
Dreknor <pauli84@freenet.de>
2026-08-12 14:08:16 +0200

    Handle SettingAlreadyExists exception in Pflichtstunden settings migration.

--- database/settings/2026_08_06_131250_add_pflichtstunden_bereiche_to_pflichtstunden_settings.php
+++ database/settings/2026_08_06_131250_add_pflichtstunden_bereiche_to_pflichtstunden_settings.php
@@ -6,7 +6,12 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('pflichtstunden.pflichtstunden_bereiche', []);
+        try {
+            $this->migrator->add('pflichtstunden.pflichtstunden_bereiche', []);
+        } catch (\Spatie\LaravelSettings\Exceptions\SettingAlreadyExists $e) {
+            // Setting already exists, do nothing
+            \Illuminate\Support\Facades\Log::info('Setting already exists: ' . $e->getMessage());
+        }
     }
 
     public function down(): void
