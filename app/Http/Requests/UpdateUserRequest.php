8527f590550f8dbd0d87930c98c1eca8e48931a8
Dreknor <pauli84@freenet.de>
2026-08-24 11:13:32 +0200

    Refactor validation rules in `UpdateUserRequest` to use `Rule::unique` with `withoutTrashed`.

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Model\User;
-use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules\Password;
 
