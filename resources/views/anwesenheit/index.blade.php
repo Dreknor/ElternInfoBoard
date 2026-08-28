fc156db06fa392f880964d895a10558883afbc72
Dreknor <pauli84@freenet.de>
2026-08-28 15:41:58 +0200

    Add support for displaying `publicPhone` for parents and partners across views and controller

--- resources/views/anwesenheit/index.blade.php
+++ resources/views/anwesenheit/index.blade.php
@@ -437,10 +437,14 @@
                                 const emailLink = p.email
                                     ? ` &nbsp;<a href="mailto:${p.email}" class="text-muted small"><i class="fas fa-envelope mr-1"></i>${p.email}</a>`
                                     : '';
-                                const phoneLink = p.phone
-                                    ? ` &nbsp;<a href="tel:${p.phone}" class="text-muted small"><i class="fas fa-phone mr-1"></i>${p.phone}</a>`
-                                    : '';
-                                li.innerHTML = '<i class="fas fa-user mr-2"></i><b>' + (p.name || '') + '</b>' + emailLink + phoneLink;
+                                let phoneLinks = '';
+                                if (p.phone) {
+                                    phoneLinks += ` &nbsp;<a href="tel:${p.phone}" class="text-muted small"><i class="fas fa-phone mr-1"></i>${p.phone}</a>`;
+                                }
+                                if (p.publicPhone && p.publicPhone !== p.phone) {
+                                    phoneLinks += ` &nbsp;<a href="tel:${p.publicPhone}" class="text-muted small"><i class="fas fa-phone mr-1"></i>${p.publicPhone}</a>`;
+                                }
+                                li.innerHTML = '<i class="fas fa-user mr-2"></i><b>' + (p.name || '') + '</b>' + emailLink + phoneLinks;
                                 elternContainer.appendChild(li);
                             });
                         } else {
